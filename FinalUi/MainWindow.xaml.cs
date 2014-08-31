cb6b6f58d042c5760ab5450b89e5be9cca373c8a
mohd alishan khan <tecxnitrom@gmail.com>
2014-08-31 14:28:31 +0530

    first commit

---
+++ FinalUi/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DataGridHelper dataGridHelper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGridHelper = new DataGridHelper();
+        }
+        public MainWindow(string userName)
+        {
+
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            LoadData loadData = new LoadData();
+            loadData.Closed += loadData_Closed;
+            loadData.Show();
+        }
+
+        void loadData_Closed(object sender, EventArgs e)
+        {
+            if (((LoadData)sender).dataLoaded)
+            {
+
+            }
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
