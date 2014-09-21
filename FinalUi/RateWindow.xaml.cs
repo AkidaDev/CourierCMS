a7b80b99290e30ccbc75d8d636b9700c71d74b57
mohd alishan khan <tecxnitrom@gmail.com>
2014-09-21 18:45:04 +0530

    hkhk

--- FinalUi/RateWindow.xaml.cs
+++ FinalUi/RateWindow.xaml.cs
@@ -24,13 +24,11 @@ namespace FinalUi
         CollectionViewSource ServiceList;
         CollectionViewSource RateList;
         bool isEdited;
+        Rate rate;
         public RateWindow()
         {
             InitializeComponent();
             BillingDataDataContext db = new BillingDataDataContext();
-            // ZoneList.Source = db.ZONEs.Select(x => x.zcode);
-            //ServiceList.Source = db.Services.Select(x => x.SER_CODE);
-            //RateList.Source = db.ZONEs.Select(x => x.zcode);
             isEdited = false;
         }
 
@@ -45,47 +43,7 @@ namespace FinalUi
                 ComboBoxService.SelectedValue = assign.ServiceCode;
             }
         }
-        private void executeFillNewRatePanel()
-        {
-        }
-        private void fillPanel(XElement rate)
-        {
-
-        //    RatePanel.Children.RemoveRange(0, RatePanel.Children.Count - 1);
-        //    foreach (var item in rate.Elements())
-        //    {
-        //        WrapPanel rateNodeTypePanel = new WrapPanel();
-        //        TextBlock textBlock = new TextBlock();
-        //        textBlock.Text = "Select Type: ";
-        //        rateNodeTypePanel.Children.Add(textBlock);
-        //        ComboBox ComboBoxSelectType = new ComboBox();
-        //        ComboBoxSelectType.Items.Add("Range");
-        //        ComboBoxSelectType.Items.Add("Step");
-        //        ComboBoxSelectType.SelectionChanged += ComboBoxSelectType_SelectionChanged;
-        //        rateNodeTypePanel.Children.Add(ComboBoxSelectType);
-        //        TextBlock weightTextBlock = new TextBlock();
-        //        weightTextBlock.Text = "Enter TillWeight: ";
-        //        rateNodeTypePanel.Children.Add(weightTextBlock);
-        //        TextBox weightTextBox = new TextBox();
-        //        rateNodeTypePanel.Children.Add(weightTextBox);
-        //        weightTextBox.Tag = "Weight";
-        //        TextBlock priceTextBlock = new TextBlock();
-        //        priceTextBlock.Text = "Enter Price: ";
-        //        rateNodeTypePanel.Children.Add(priceTextBlock);
-        //        TextBox priceTextBox = new TextBox();
-        //        priceTextBox.Tag = "Price";
-        //        rateNodeTypePanel.Children.Add(priceTextBox);
-        //        if (item.Attribute("type").Value == "step")
-        //        {
-        //            TextBlock stepTextBlock = new TextBlock();
-        //            stepTextBlock.Text = "Enter StepWeight: ";
-        //            rateNodeTypePanel.Children.Add(stepTextBlock);
-        //            TextBox stepTextBox = new TextBox();
-        //            stepTextBox.Tag = "StepWeight";
-        //            rateNodeTypePanel.Children.Add(stepTextBox);
-        //        }
-        //    }
-        }
+        
         void ComboBoxSelectType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             throw new NotImplementedException();
